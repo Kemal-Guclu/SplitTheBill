43d0d3afc5bbc20d28c60efb52fe32bddf4fd391
Kemal-Guclu <kemal.guclu@chasacademy.se>
2024-10-27 15:08:45 +0100

    added css and js files and completed

---
+++ script.js
@@ -0,0 +1,20 @@
+const numOfFriends = document.querySelector("#num-of-friends");
+const totalSum = document.querySelector("#total-sum");
+const tip = document.querySelector("#tip");
+const calc = document.querySelector(".calculate");
+
+const sumPerson = document.querySelector(".sum-per-person");
+const restCharity = document.querySelector(".rest-for-charity");
+
+function calculate() {
+  const friends = parseInt(numOfFriends.value);
+  const sum = parseFloat(totalSum.value);
+  const tipValue = parseFloat(tip.value);
+
+  const sumPerPerson = Math.floor((sum + tipValue) / friends);
+  /*const restForCharity = sum + tipValue - sumPerPerson * friends;*/
+  const restForCharity = (sum + tipValue) % friends;
+
+  sumPerson.textContent = `${sumPerPerson} kr`;
+  restCharity.textContent = `${restForCharity} kr`;
+}
